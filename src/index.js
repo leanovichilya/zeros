aaa3d70e1bab52834bcf4a52a1d396504a42d346
Ilya <53150979+leanovichilya@users.noreply.github.com>
2019-10-10 13:04:22 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
-module.exports = function zeros(expression) {
-  // your solution
+function countFives(num) {
+  let count = 0;
+
+  while (num % 5 === 0) {
+    num /= 5;
+    count += 1;
+  }
+  return count;
 }
+
+function countDeuces(num) {
+  let count = 0;
+
+  while (num % 2 === 0) {
+    num /= 2;
+    count += 1;
+  }
+  return count;
+}
+
+function countExclamationPoint(str) {
+  return str.endsWith('!!') ? 2 : 1;
+}
+
+module.exports = function zeros(expression) {
+  let arrayNumbers = expression.split('*');
+  let twoCount = 0;
+  let fiveCount = 0;
+
+  for (let i = 0; i < arrayNumbers.length; i += 1) {
+    let exclamationPoint = countExclamationPoint(arrayNumbers[i]);
+    arrayNumbers[i] = parseInt(arrayNumbers[i], 10);
+
+    for (;arrayNumbers[i] > 0; arrayNumbers[i] -= exclamationPoint) {
+      let buffer = arrayNumbers[i];
+      fiveCount += countFives(buffer);
+      twoCount += countDeuces(buffer);
+    }
+  }
+
+  return twoCount < fiveCount ? twoCount : fiveCount;
+};
